5a1868bce52e2c1c69902a4252042751aedb97de
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2024-07-11 09:27:28 -0400

    chore: fix stack pointer starting value

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use processor_status::ProcessorStatus;
 use sonic_rs::{Deserialize, Serialize};
 use instruction::execution::system::InterruptState;
 
-pub const STACK_POINTER_STARTING_VALUE: u8 = 0xFD;
+pub const STACK_POINTER_STARTING_VALUE: u8 = 0x00;
 pub const NMI_VECTOR_ADDRESS: u16 = 0xFFFA;
 pub const RESET_VECTOR_ADDRESS: u16 = 0xFFFC;
 pub const IRQ_BRK_VECTOR_ADDRESS: u16 = 0xFFFE;
